15ec025d4125d82d21d27a7c3fe64399ee79a62e
Barthelemy Renucci <barthelemy.renucci@zoov.eu>
2019-08-19 10:08:04 +0200

    [init][custom-keybinding] Added some custom keybinding

---
+++ lisp/init-custom-keybindings.el
@@ -0,0 +1,63 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+
+;; {{ Modified C-h to delete
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+;; }}
+
+;; {{ Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+;; }}
+
+;; {{ Custom project jump
+(global-set-key (kbd "C-x C-²")
+                (lambda()
+                  (interactive)
+                  (dired "~/.emacs.d")))
+
+(global-set-key (kbd "C-x C-&")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware")))
+
+(global-set-key (kbd "C-x C-é")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware/modules")))
+
+(global-set-key (kbd "C-x C-\"")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware/project/main02")))
+
+(global-set-key (kbd "C-x C-'")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware/modules/includes")))
+
+(global-set-key (kbd "C-x C-(")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware/tools")))
+
+(global-set-key (kbd "C-x C--")
+                (lambda()
+                  (interactive)
+                  (dired "~/birota-repo/embedded-firmware/external")))
+;; }}
+
+;; {{ Makefile mode for custom makefile extensions
+(setq auto-mode-alist
+      (cons '("\\.inc\\'" . makefile-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.docker\\'" . makefile-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.release\\'" . makefile-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.proto\\'" . protobuf-mode) auto-mode-alist))
+;; }}
+
+(provide 'init-custom-keybindings)
