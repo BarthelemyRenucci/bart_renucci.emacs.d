2cd6f74bcdb71b06b11c6350d1c45ce3b89d933e
Barthelemy Renucci <barthelemy.renucci@zoov.eu>
2019-10-11 11:34:36 +0200

    [init-misc] Added some transparency to emacs window

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1255,4 +1255,13 @@ See https://github.com/RafayGhafoor/Subscene-Subtitle-Grabber."
 (zone-when-idle 120)
 ;; }}
 
+;; {{ Add transparency
+ ;; Set transparency of emacs
+ (defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
+(transparency 95)
+;; }}
+
 (provide 'init-misc)
